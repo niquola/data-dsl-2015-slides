9a7d33410a39b45cfdbbccf8f6fb50af9243e7f3
niquola <niquola@gmail.com>
2015-08-08 19:41:30 +0000

    update

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [hiccup "1.0.5"]
+                 [route-map "0.0.1"]
+                 [prismatic/plumbing "0.4.4"]
                  [prismatic/schema "0.4.3"]
                  [honeysql "0.6.1"]])
